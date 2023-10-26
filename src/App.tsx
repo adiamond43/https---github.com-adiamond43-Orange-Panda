de8c1868a6f1246df0a81a6e3dc66ddb0eb5c4b4
Andy Diamond <adiamond2002@gmail.com>
2023-10-26 11:02:03 -0400

    git push

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,62 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import Home from "./Home/Home";
+import Products from "./Products/Products";
 
 function App() {
+  const openMenu = () => {
+    document.querySelector(".sidebar")?.classList.add("open");
+  }
+  const closeMenu = () => {
+    document.querySelector(".sidebar")?.classList.remove("open");
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+    <div className="grid-container">
+        <link rel="stylesheet" href="index.css">
+        <title>Orange Panda</title>
+
+        <body>
+            <div>
+                <header className="header">
+                    <div className="brand">
+                        <button onClick={openMenu}>&#9776;</button>
+                        <a href="index.html">Orange Panda</a>
+                    </div>
+                    <div className=".header-links">
+                        <Link to="/">Home</Link>
+                        <Link to="/catalog">Catalog</Link>
+                    </div>
+                </header>
+                <aside className="sidebar">
+                    <h3>Shopping Categories</h3>
+                    <button className="sidebar-close-button" onClick={closeMenu}>x</button>
+                    <ul>
+                        <li>
+                            <a href="index.html">Pants</a>
+                        </li>
+
+                        <li>
+                            <a href="index.html">Shirts</a>
+                        </li>
+                    </ul>
+                </aside>
+                <main className="main">
+                    <Routes>
+                      <Route path="/" element={<Home />} />
+                      <Route path="/catalog" element={<Products />} />
+                    </Routes>
+                </main>
+                <footer className="footer">
+                    &copy; 2022 Orange Panda
+                </footer>
+            </div>
+        </body>
+        </link>
     </div>
+    </Router>
   );
 }
 
